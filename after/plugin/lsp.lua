228005e8e3fc01f80307318ea663847862003f0c
Reggie Thomas <reggie@reggies-mbp.lan>
2023-11-16 01:33:46 -0800

    Update deps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,12 +1,18 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
-lsp.ensure_installed({
-    'tsserver',
-    'eslint',
-    'sumneko_lua',
-    'rust_analyzer',
-    'gopls',
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    ensure_installed = {    
+        'tsserver',
+        'eslint',
+        'lua_ls',
+        'rust_analyzer',
+        'gopls',
+    },
+    handlers = {
+      lsp.default_setup,
+    },
 })
 
 lsp.set_preferences({
