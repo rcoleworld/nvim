82b085144aaf850ed89617c17809a068b618fb6a
Reggie Thomas <reggie@reggies-mbp.lan>
2023-11-16 23:02:12 -0800

    Enable tab completion

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,18 +1,30 @@
 local lsp = require('lsp-zero')
+local cmp = require('cmp')
+local cmp_action = lsp.cmp_action()
+
 lsp.preset('recommended')
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {    
+    ensure_installed = {
         'tsserver',
         'eslint',
         'lua_ls',
         'rust_analyzer',
         'gopls',
+        'denols',
     },
     handlers = {
       lsp.default_setup,
     },
+})
+
+cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+        ['<Tab>'] = cmp_action.tab_complete(),
+        ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+    })
 })
 
 lsp.set_preferences({
